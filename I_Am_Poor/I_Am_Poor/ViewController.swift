af001ef51729aa92040e1ba941f8b2c2a0701fca
SANAY U J <67674714+sanay2255@users.noreply.github.com>
2022-08-19 12:26:08 +0530

    Add files via upload

---
+++ I_Am_Poor/I_Am_Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_Am_Poor
+//
+//  Created by sanay uj on 18/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
