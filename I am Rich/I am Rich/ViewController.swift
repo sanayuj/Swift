8f59daf3533ef8016d958cde0c5fc8ef1e6591d3
SANAY U J <67674714+sanay2255@users.noreply.github.com>
2022-08-19 12:23:44 +0530

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by sanay uj on 16/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
